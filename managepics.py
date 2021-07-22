aa4c830155e9a5f753241bd7182a6b3ea583b1f0
Jhanvi Patel <jhanvipatel167@gmail.com>
2021-07-21 23:37:32 -0400

    added fuctionality to check outputdirs and create if not present

--- managepics.py
+++ managepics.py
@@ -1,36 +1,85 @@
+import configparser
 import glob
+import re
+import os
+
 from PIL import Image
 from PIL.ExifTags import TAGS
-import re
 
-def get_image_datetime (filename):
+
+def check_output_directories(output_dirs): 
+
+    for dir in output_dirs:
+        if not os.path.exists(output_dirs[dir]): 
+            os.makedirs(output_dirs[dir])
+    
+
+
+DATETIME_TAG = 306
+
+def get_filename(image_datetime, extension = 'jpg'):
+    output_filename = re.sub(':', '', image_datetime)
+    output_filename = re.sub(' ', '_', output_filename)
+    return f'{output_filename}.{extension}'
+
+def get_image_datetime (filename, datetime_tag=306) :
     image = Image.open(filename)
     exifdata = image.getexif()
     
     datetime = ''
 
     #tag 306 is Datetime
-    if 306 in exifdata.keys():
-        datetime = exifdata.get(306)
-        # if isinstance(datetime, bytes):
-        #     datetime = datetime.decode()
-
+    if datetime_tag in exifdata.keys():
+        datetime = exifdata.get(datetime_tag)
+        
     return datetime
 
-def get_filename(image_datetime):
-    output_filename = re.sub(':', '', image_datetime)
-    output_filename = re.sub(' ', '_', output_filename)
-    return output_filename
+# def get_image_filesize(filename, filesize_tag=514):
+#     # 514 is number of bytes of jpeg data
+#     image = Image.open(filename)
+#     exifdata = image.getexif()
+#     filesize = ''
 
-for filename in glob.iglob('pics\**\*.jpg', recursive=True):
-    image_datetime = get_image_datetime(filename)
-   
-    if image_datetime:
-        output_filename = get_filename(image_datetime)
-     
-    print(f'{filename}: {output_filename}')
+#     if filesize_tag in exifdata.keys():
+#         filesize = exifdata.get(filesize_tag)
+        
+    
+
+def main():
+    
+    configs = configparser.ConfigParser()
+    configs.read('configs.ini')
 
 
+    input_files_pattern = f"{configs['INPUT']['pics.directory']}\**\*.jpg"
+
+    #check if output directories exist, if not, create them.
+    check_output_directories(configs['OUTPUT'])
+
+    for filename in glob.iglob(input_files_pattern, recursive=True):
+        image_datetime = get_image_datetime(filename)
+        output_filename = ''
+        if image_datetime:
+            output_filename = get_filename(image_datetime)
+        
+        print(f'{filename}: {output_filename}')
+
+
+    # organized = {}
+    # duplicates = {}
+    # scanned = {}
+    
+    # if output_filename == '':
+    #     scanned.add(output_filename)
+    # else:
+    #     organized.add(output_filename)
+    #     if output_filename in organized and filesize :
+
+
+
+if __name__ == '__main__':
+     main()
+    
 
 
 # from PIL import Image
