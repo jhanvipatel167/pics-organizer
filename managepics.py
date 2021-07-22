0a02f7b3453b927778c19570cddd4ce02798d65e
Jhanvi Patel <jhanvipatel167@gmail.com>
2021-07-22 03:39:28 -0400

    updates to organize pics

--- managepics.py
+++ managepics.py
@@ -2,24 +2,32 @@ import configparser
 import glob
 import re
 import os
+import shutil
 
 from PIL import Image
 from PIL.ExifTags import TAGS
 
+def is_same_size(srcfile, dstfile):
+    if os.path.getsize(srcfile) == os.path.getsize(dstfile):
+        return True
+    else:
+        return False
+
 
 def check_output_directories(output_dirs): 
-
+    
     for dir in output_dirs:
         if not os.path.exists(output_dirs[dir]): 
             os.makedirs(output_dirs[dir])
-    
-
-
-DATETIME_TAG = 306
 
 def get_filename(image_datetime, extension = 'jpg'):
+
+    # remove ':' from the date time string
     output_filename = re.sub(':', '', image_datetime)
+
+    #replace space with underscore
     output_filename = re.sub(' ', '_', output_filename)
+    
     return f'{output_filename}.{extension}'
 
 def get_image_datetime (filename, datetime_tag=306) :
@@ -33,69 +41,65 @@ def get_image_datetime (filename, datetime_tag=306) :
         datetime = exifdata.get(datetime_tag)
         
     return datetime
-
-# def get_image_filesize(filename, filesize_tag=514):
-#     # 514 is number of bytes of jpeg data
-#     image = Image.open(filename)
-#     exifdata = image.getexif()
-#     filesize = ''
-
-#     if filesize_tag in exifdata.keys():
-#         filesize = exifdata.get(filesize_tag)
-        
+       
     
 
 def main():
     
+    #read the configurations
     configs = configparser.ConfigParser()
     configs.read('configs.ini')
-
 
     input_files_pattern = f"{configs['INPUT']['pics.directory']}\**\*.jpg"
 
     #check if output directories exist, if not, create them.
     check_output_directories(configs['OUTPUT'])
 
+
+    images_organized = set()
+
+    scanned_dir = configs['OUTPUT']['scanned.directory']
+    dups_dir = configs['OUTPUT']['duplicates.directory']
+    organized_dir = configs['OUTPUT']['organized.directory']
+
     for filename in glob.iglob(input_files_pattern, recursive=True):
         image_datetime = get_image_datetime(filename)
         output_filename = ''
         if image_datetime:
             output_filename = get_filename(image_datetime)
-        
-        print(f'{filename}: {output_filename}')
+
+            if output_filename in images_organized:
+                dstfile = f"{organized_dir}\{output_filename}"
+
+                #compare file sizes
+                if is_same_size(filename, dstfile):
+                    shutil.copy(filename, dups_dir)
+                else:
+                    print('tricky situation')
+            
+            else:
+                images_organized.add(output_filename)
+
+                destfile = f'{organized_dir}\{output_filename}'
+                shutil.copyfile(filename, destfile)
+
+            '''
+            -check if output filename already exists in images_organized
+                -if its there then compare the file sizes to be sure we are dealing with different images
+                    -if the file sizes are same, its a duplicate, move the recent image to duplicates dir
+                    -if the file sizes are diff, its not a duplicate, move the recent image to organized dir
+                -if its not there, 
+                    -add the output_filename in image_organized set
+                    -copy the output_filename to organized dir
+            
+            '''
 
 
-    # organized = {}
-    # duplicates = {}
-    # scanned = {}
-    
-    # if output_filename == '':
-    #     scanned.add(output_filename)
-    # else:
-    #     organized.add(output_filename)
-    #     if output_filename in organized and filesize :
+        else:
+            # its a scanned image
+            shutil.copy(filename, scanned_dir)
 
-
+        #print(f'{filename}: {output_filename}')
 
 if __name__ == '__main__':
      main()
-    
-
-
-# from PIL import Image
-# from PIL.ExifTags import TAGS
-# # path to the image or video
-# imagename = r"C:\Users\14706\Documents\picsproject\pics\EP-1gb\100NCD40\Kiddies\lw.jpg"
-# # read the image data using PIL
-# image= Image.open(imagename)
-# # extract EXIF data
-# exifdata= image.getexif()
-# # iterating over all EXIF data fields
-# for tag_id in exifdata:
-#     # get the tag name, instead of human unreadable tag id
-#     tag= TAGS.get(tag_id, tag_id)
-#     data = exifdata.get(tag_id)
-#     # decode bytes 
-#     if isinstance(data, bytes):
-#         data= data.decode()
-#     print(f"{tag:25}: {data}')")
